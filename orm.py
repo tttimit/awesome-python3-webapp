4c4846856f391ee6270d03589aa6bc83607f04f4
tttimit <timit.cloud@gmail.com>
2016-08-31 23:08:32 +0800

    commit 2

---
+++ orm.py
@@ -0,0 +1,293 @@
+import asyncio
+
+import aiomysql
+import logging
+
+from orm import Model, StringField, IntegerField
+
+__pool
+
+
+def log(sql, args=()):
+    logging.info('SQL: %s' % sql)
+
+
+@asyncio.coroutine
+def create_pool(loop, **kw):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = yield from aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw.get['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset', 'utf-8'),  # 默认编码为 utf-8
+        autocommit=kw.get('autocommit', True),  # 自动提交事务
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
+        loop=loop
+    )
+
+
+@asyncio.coroutine
+def select(sql, args, size=None):
+    logging.log(sql, args)
+    global __pool
+    with (yield from __pool) as conn:
+        cur = yield from conn.cursor(aiomysql.DictCursor)
+        yield from cur.execute(sql.replace('?', '%s'), args or ())  # SQL的占位符是 ?  MySQL的则是 %s
+        if size:
+            rs = yield from cur.fetchmany(size)
+        else:
+            rs = yield from cur.fetchall()
+        yield from cur.close()
+        logging.info('rows returned: %s' % len(rs))
+        return rs
+
+
+@asyncio.coroutine
+def execute(sql, args):
+    logging.log(sql)
+    with (yield from __pool) as conn:
+        try:
+            cur = yield from conn.cursor()
+            yield from cur.execute(sql.replace('?', '%s'), args)
+            affected = cur.rowcount
+            yield from cur.close()
+        except BaseException as e:
+            raise
+        return affected  # 与查询不同，execute只返回影响的结果数
+
+
+def create_args_string(num):
+    L = []
+    for n in range(num):
+        L.append('?')
+    return ', '.join(L)
+
+
+class ModelMetaclass(type):
+    """
+    metaclass 在创建类的时候起作用
+    __new__ 在实例创建的时候起作用
+    __init__ 在实例初始化的时候做一些小手脚
+    这个元类是干啥的呢？
+    它为继承自Model但不是Model的类添加了额外的功能
+    比如额外判断一下primaryKey是否重复
+    然后为这个类添加了增删改查的方法，然后其他该有的属性都还有，只是额外添加了一些功能和判断
+    后续任何继承自Model的类，比如我们的User类，会自动通过ModelMetaclass扫描映射关系，并存储到
+    自身的类属性例如 __table__  __mappings__ 中。
+    """
+
+    def __new__(cls, name, bases, attrs):  # name 是类的名称，bases是一个tuple，表示继承自哪些类，attrs表示属性
+        # 排除掉Model类本身
+        if name == 'Model':
+            return type.__new__(cls, name, bases, attrs)  # 这个是默认的创建实例的方法
+        # 获取table的名字：
+        tableName = attrs.get('__table__', None) or name
+        logging.info('found model: %s (table: %s)' % (name, tableName))
+        # 获取所有的Field和主键名：
+        mappings = dict()
+        fields = []
+        primaryKey = None
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                logging.info('  found mapping: %s ==> %s' % (k, v))
+                mappings[k] = v
+                if v.primary_key:
+                    # 找到主键
+                    if primaryKey:
+                        raise RuntimeError('Duplicate primary key for field: %s' % k)
+                    primaryKey = k
+                else:
+                    fields.append(k)
+        if not primaryKey:
+            raise RuntimeError('Primary key not found.')
+        for k in mappings.keys():
+            attrs.pop(k)
+        escaped_fields = list(map(lambda f: '`%s`' % f, fields))
+        attrs['__mappings__'] = mappings  # 保存属性和列的映射关系
+        attrs['__table__'] = tableName
+        attrs['__primary_key__'] = primaryKey  # 主键的属性名
+        attrs['__field__'] = fields  # 主键之外的属性名
+        # 构造默认的增删改查语句
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (
+            tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (
+            tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+        return type.__new__(cls, name, bases, attrs)
+
+
+class Model(dict, metaclass=ModelMetaclass):
+    """
+    Model 继承自 dict，所以具备所有 dict 的功能，同事又实现了 __getattr__() 和 __setattr__()
+    因此，又可以像普通字段那样引用：
+    >>> user['id']
+    123
+    >>> user.id
+    123
+    """
+
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getValue(self, key):
+        return getattr(self, key, None)
+
+    def getValueOrDefault(self, key):
+        value = getattr(self, key, None)
+        if value is None:
+            field = self.__mappings__[key]
+            if field.default is not None:
+                value = field.default() if callable(field.default) else field.default
+                logging.debug('using default value for %s: %s' % (key, str(value)))
+                setattr(self, key, value)
+        return value
+
+    @classmethod
+    @asyncio.coroutine
+    def find(cls, pk):
+        '''
+        我们往Model类添加class方法，就可以让所有子类调用class方法
+        子类就可以通过类方法实现主键查找：
+        user = yield from User.find('123')
+        '''
+        rs = yield from select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+        if len(rs) == 0:
+            return None
+        return cls(**rs[0])
+
+    @asyncio.coroutine
+    def save(self):
+        '''
+        往Model类添加实例方法，就可以让子类调用实例方法，这样就可以把一个User实例存入数据库
+        user = User(id=123, name='Michael')
+        yield from user.save()
+        '''
+        args = list(map(self.getValueOrDefault, self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
+        rows = yield from execute(self.__insert__, args)
+        if rows != 1:
+            logging.warn('failed to insert record: affected rows: %s' % rows)
+
+    @classmethod
+    @asyncio.coroutine
+    def findAll(cls, where=None, args=None, **kw):
+        ' find objects by where clause.'
+        sql = [cls.__select__]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        if args is None:
+            args = []
+        orderBy = kw.get('orderBy', None)
+        if orderBy:
+            sql.append('order by')
+            sql.append(orderBy)
+        limit = kw.get('limit', None)
+        if limit is not None:
+            sql.append('limit')
+            if isinstance(limit, int):
+                sql.append('?')
+                args.append(limit)
+            elif isinstance(limit, tuple) and len(limit) == 2:
+                sql.append('?, ?')
+                args.extend(limit)
+            else:
+                raise ValueError('Invalid limit value: %s' % str(limit))
+        rs = yield from select(' '.join(sql), args)
+        return [cls(**r) for r in rs]
+
+    @classmethod
+    @asyncio.coroutine
+    def findNumber(cls, selectField, where=None, args=None):
+        ' find number by select and where'
+        sql = ['select %s _num_ from `%s`' % (selectField, cls.__table__)]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        rs = yield from select(' '.join(sql), args, 1)
+        if len(rs) == 0:
+            return None
+        return rs[0]['_num_']
+
+    @asyncio.coroutine
+    def update(self):
+        args = list(map(self.getValue, self.__fields__))
+        args.append(self.getValue(self.__primary_key__))
+        rows = yield from execute(self.__update__, args)
+        if rows != 1:
+            logging.warn('failed to update by primary key: affected rows: %s' % rows)
+
+    @asyncio.coroutine
+    def remove(self):
+        args = [self.getValue(self.__primary_key__)]
+        rows = yield from execute(self.__delete__, args)
+        if rows != 1:
+            logging.warn('failed to remove by primary key: affected rows: %s' % rows)
+
+
+class Field(object):
+    def __init__(self, name, column_type, primary_key, default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+
+    def __str__(self):
+        return '<%s, %s: %s>' % (self.__class__.__name__, self.column_type, self.name)
+
+
+class StringField(Field):
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
+
+
+class BooleanField(Field):
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
+
+
+class IntegerField(Field):
+    def __init__(self, name=None, primary_key=False, defalut=0):
+        super().__init__(name, 'bigint', primary_key, default)
+
+
+class FloatField(Field):
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
+
+
+class TextField(Field):
+    def __init__(self, name=None, default=None):
+        super().__init__(name, 'text', False, default)
+
+
+# 设计ORM需要从上往下思考，用户会如何使用
+class User(Model):
+    """在类级别定义属性是用来描述User对象和表的映射，实例要通过__init__()去初始化，两者互不影响
+    根据例子可以看到，每个实例其实代表了每个元组（即表中的每行记录）
+    这里强调id和name是类属性，描述了Object和Relation的映射，和每行记录没有关系
+    # 创建实例：
+    user = User(id=123, name='Michael')
+    # 存入数据库：
+    user.insert()
+    # 查询所有的User对象：
+    users = User.findAll()
+    """
+    __table__ = 'users'
+
+    id = IntegerField(primary_key=True)  #
+    name = StringField()  #
